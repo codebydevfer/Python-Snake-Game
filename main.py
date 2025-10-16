3e31121ece889487f52e7b26888d72c540d2429c
Fernando <fernandopeandrade@outlook.com>
2025-10-16 10:15:21 -0400

    Fruit and snake collision fixed

--- main.py
+++ main.py
@@ -30,15 +30,15 @@ fps = pygame.time.Clock()
 snake_position = [100, 50]
 
 # first 4 blocks of snake body
-snake_body = [ [100, 50],
+snake_body = [[100, 50],
               [90, 50],
               [80, 50],
               [70, 50]
               ]
 
 # fruit position
-fruit_position = [random.randrange(1, (window_x//10) * 10),
-                  random.randrange(1, (window_y//10) * 10)]
+fruit_position = [random.randrange(1, (window_x//10)) * 10,
+                  random.randrange(1, (window_y//10)) * 10]
 
 fruit_spawn = True
 
@@ -134,16 +134,15 @@ while True:
         snake_body.pop()
         
     if not fruit_spawn:
-        fruit_position = [random.randrange(1, (window_x//10)) * 10, 
+        fruit_position = [random.randrange(1, (window_x//10)) * 10,
                           random.randrange(1, (window_y//10)) * 10]
         
     fruit_spawn = True
     game_window.fill(black)
     
     for pos in snake_body:
-        pygame.draw.rect(game_window, green, pygame.Rect(
-          pos[0], pos[1], 10, 10))
-        
+        pygame.draw.rect(game_window, green, 
+                         pygame.Rect(pos[0], pos[1], 10, 10))
     pygame.draw.rect(game_window, white, pygame.Rect(
       fruit_position[0], fruit_position[1], 10, 10))
 
